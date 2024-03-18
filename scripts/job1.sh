81f79d2f5b3000b12297373abe6e206c710e77c9
murali rayala <murali.rayala22@gmail.com>
2024-03-18 12:41:05 +0000

    Update job1.sh

--- scripts/job1.sh
+++ scripts/job1.sh
@@ -2,7 +2,6 @@
 
 # This is a first shell script
 
-echo "This script is running in a GitLab CI/CD pipeline."
-echo "The current directory is: $(pwd)"
-echo "The contents of the directory are:"
-ls -l
+echo "Gold saamii asked me to run this script in a GitLab CI/CD pipeline."
+# Display the current date and time
+echo "Current date and time: $(date)"
