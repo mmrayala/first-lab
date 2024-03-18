50d33ee38414abac6b2c8fdb9b57f2e0ddc6448e
murali rayala <murali.rayala22@gmail.com>
2024-03-18 12:33:49 +0000

    Add new file

---
+++ scripts/job1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a first shell script
+
+echo "This script is running in a GitLab CI/CD pipeline."
+echo "The current directory is: $(pwd)"
+echo "The contents of the directory are:"
+ls -l
